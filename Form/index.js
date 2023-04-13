8e078c4511e08489dda0e3dacbeecb318f128d3e
Anmol Tiwari <96834701+anmoltiwari0712@users.noreply.github.com>
2023-04-13 11:14:08 +0530

    Add files via upload

---
+++ Form/index.js
@@ -0,0 +1,93 @@
+function myFunction() {
+  let text;
+  let State = prompt("Enter Your State: ");
+  switch(State) {
+    case "Andhra Pradesh":
+        alert("Email-id: cdma_ap@cdma.gov.in");
+        break;
+    case "Arunachal Pradesh":
+        alert("Email-id:  imcitanagar@gmail.com");
+        break;
+    case "Assam":
+        alert("Email-id:  guwahaticom@gmail.com");
+        break;
+    case "Bihar":
+        alert("Email-id: pmcprda@gmail.com");
+        break;
+    case "Chhattisgarh":
+        alert("Email-id: ");
+        break;
+    case "Goa":
+        alert("Email-id: ");
+        break;
+    case "Gujarat":
+        alert("Email-id: dmc.jnv@suratmunicipal.gov.in");
+        break;
+    case "Haryana":
+        alert("Email-id: dulbhry@hry.nic.in");
+        break;
+    case "Himachal Pradesh":
+        alert("Email-id: mcs_shimla@yahoo.com");
+        break;
+    case "Jammu and Kashmir":
+        alert("Email-id: jmcjammu@rediffmail.com");
+        break;
+    case "Jharkhand":
+        alert("Email-id: jh.support_2@sparrowsoftech.net");
+        break;
+    case "Karnataka":
+        alert("Email-id: jdr@mrc.gov.in");
+        break;
+    case "Kerala":
+        alert("Email-id:");
+        break;
+    case "Madhya Pradesh":
+        alert("Email-id: employeehelpdesk@mpurban.gov.in");
+        break;
+    case "Maharashtra":
+        alert("Email-id: contactus@punecorporation.org");
+        break;
+    case "Manipur":
+        alert("Email-id: imcmunicouncil@gmail.com");
+        break;
+    case "Meghalaya":
+        alert("Email-id: smb-meg@nic.in");
+        break;
+    case "Mizoram":
+        alert("Email-id: amcmizoram@gmail.com");
+        break;
+    case "Nagaland":
+        alert("Email-id:");
+        break;
+    case "Orissa":
+        alert("Email-id:");
+        break;
+    case "Punjab":
+        alert("Email-id: kmc-ngl@gov.in");
+        break;
+    case "Rajasthan":
+        alert("Email-id: udh@rajasthan.gov.in");
+        break;
+    case "Sikkim":
+        alert("Email-id: gmc.sikkim@gmail.com");
+        break;
+    case "Tamil Nadu":
+        alert("Email-id: cma.tncma@nic.in");
+        break;
+    case "Uttar Pradesh":
+        alert("Email-id: bdmfr-up@gov.in");
+        break;
+    case "Telangana":
+        alert("Email-id: cdmasupport@cgg.gov.in");
+        break;
+    case "Tripura":
+        alert("Email-id: amc.tripura@gmail.com");
+        break;
+    case "West Bengal":
+        alert("Email-id: secy.ma-wb@gov.in");
+        break;    
+    default:
+      alert("Not Found Try Again")
+  }
+//   document.getElementById("demo").innerHTML = text;
+}
